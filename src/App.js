09c9e635c55f711802f33c355134cbe32cc7d8d4
Manjeet Gupta <manjeetguptaemployid@gmail.com>
2025-01-02 15:41:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+iimport React from 'react';
+import LighthouseAudit from './Components/lighthouseAudit';
+import './App.css'
+function App() {
+  return (
+    <div className="App"> 
+      <LighthouseAudit />
+    </div>
+  );
+}
+
+export default App;
